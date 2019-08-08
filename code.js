240f524669f4f2b299b9237d557b93694a701d95
Thomas Lowry <tom@negativespace.ca>
2019-08-08 09:18:19 -0400

    Initial commit

---
+++ code.js
@@ -0,0 +1,101 @@
+figma.showUI(__html__, { width: 248, height: 176 });
+//
+// Ordering functions
+//
+//sort the stack order by top to bottom, left to right
+function sortPosition(nodeData) {
+    nodeData.sort(function (node1, node2) {
+        if (node1.y < node2.y)
+            return 1;
+        if (node1.y > node2.y)
+            return -1;
+        if (node1.x < node2.x)
+            return 1;
+        if (node1.x > node2.x)
+            return -1;
+    });
+    return nodeData;
+}
+//sort the stack order alphabetically, ascending or descending
+function sortAlpha(nodeData, direction) {
+    nodeData.sort(function (node1, node2) {
+        if (node1.name < node2.name)
+            return 1;
+        if (node1.name > node2.name)
+            return -1;
+    });
+    switch (direction) {
+        case "asc":
+            return nodeData;
+            break;
+        case "desc":
+            nodeData = nodeData.reverse();
+            return nodeData;
+            break;
+    }
+}
+//reverse the stack order of selection
+function sortReverse(nodeData) {
+    return nodeData.map(function (item, idx) { return nodeData[nodeData.length - 1 - idx]; });
+}
+//randomize the stack order
+function sortRandom(nodeData) {
+    var _a;
+    for (var i = nodeData.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        _a = [nodeData[j], nodeData[i]], nodeData[i] = _a[0], nodeData[j] = _a[1];
+    }
+    return nodeData;
+}
+//
+// Helper functions
+//
+//organize nodes into arrays based on common parent
+function organizeNodesByParent(nodes) {
+    var groupedNodes = nodes.reduce(function (acc, item) {
+        if (!acc[item.parent.id]) {
+            acc[item.parent.id] = [];
+        }
+        acc[item.parent.id].push(item);
+        return acc;
+    }, {});
+    return groupedNodes;
+}
+//insert nodes
+function insertNodes(parent, nodes) {
+    for (var i = 0; i < nodes.length; i++) {
+        parent.insertChild(i, nodes[i]);
+    }
+}
+figma.ui.onmessage = function (msg) {
+    var selection = Array.from(figma.currentPage.selection);
+    var sortOrder = msg.order;
+    if (selection.length == 0) {
+        alert('Please make a selection.');
+    }
+    var organizedNodes = organizeNodesByParent(selection);
+    Object.keys(organizedNodes).forEach(function (group) {
+        var item = group.toString();
+        var groupedNodes = organizedNodes[item];
+        var orderedNodes = [];
+        var parent = organizedNodes[item][0].parent;
+        console.log(groupedNodes);
+        if (sortOrder == 'sortPosition') {
+            orderedNodes = sortPosition(groupedNodes);
+        }
+        else if (sortOrder == 'sortAlphaAsc') {
+            orderedNodes = sortAlpha(groupedNodes, 'asc');
+        }
+        else if (sortOrder == 'sortAlphaDesc') {
+            orderedNodes = sortAlpha(groupedNodes, 'desc');
+        }
+        else if (sortOrder == 'sortReverse') {
+            orderedNodes = sortReverse(groupedNodes);
+        }
+        else {
+            orderedNodes = sortRandom(groupedNodes);
+        }
+        console.log(orderedNodes);
+        insertNodes(parent, orderedNodes);
+    });
+};
